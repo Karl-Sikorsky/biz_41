de1e2da7ac28bfb07368a9a01a64948dd3ae5732
Karl-Sikorsky <Kalevych_tech@ukr.net>
2017-12-07 12:45:52 +0200

    localization commit

--- app/src/main/java/com/example/biz_41/BizApplication.java
+++ app/src/main/java/com/example/biz_41/BizApplication.java
@@ -1,6 +1,7 @@
 package com.example.biz_41;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import io.realm.Realm;
 
@@ -8,7 +9,7 @@ import io.realm.Realm;
  * Created by ПОДАРУНКОВИЙ on 19.10.2017.
  */
 
-public class BizApplication extends Application {
+public class BizApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
